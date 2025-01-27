a55d5025315bc49bb25facae921c009bbbd9ba55
Loïc <loic.legros.dns@gmail.com>
2025-01-27 21:53:21 +0100

    feat: add takePhoto function

---
+++ app/src/main/java/com/android/tiltcamera/camera/domain/utils.kt
@@ -0,0 +1,48 @@
+package com.android.tiltcamera.camera.domain
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.LifecycleCameraController
+import androidx.core.content.ContextCompat
+import timber.log.Timber
+
+fun takePhoto(
+    context: Context,
+    controller: LifecycleCameraController,
+    onPhotoTaken: (Bitmap) -> Unit
+) {
+    controller.takePicture(
+        ContextCompat.getMainExecutor(context),
+        object : ImageCapture.OnImageCapturedCallback() {
+            override fun onCaptureSuccess(image: ImageProxy) {
+                super.onCaptureSuccess(image)
+                Timber.d("onCaptureSuccess")
+
+                val matrix = Matrix().apply {
+                    postRotate(image.imageInfo.rotationDegrees.toFloat())
+                }
+                val rotatedBitmap = Bitmap.createBitmap(
+                    image.toBitmap(),
+                    0,
+                    0,
+                    image.width,
+                    image.height,
+                    matrix,
+                    true
+                )
+
+                onPhotoTaken(rotatedBitmap)
+                image.close()
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                super.onError(exception)
+                Timber.e(exception)
+            }
+        }
+    )
+}
