30b418d67e63c6188788c49b252b6f2572cddb3c
Loïc <loic.legros.dns@gmail.com>
2025-01-27 15:35:42 +0100

    add navigation

--- app/src/main/java/com/android/tiltcamera/MainActivity.kt
+++ app/src/main/java/com/android/tiltcamera/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.android.tiltcamera
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -19,15 +23,45 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.android.tiltcamera.ui.theme.TiltCameraTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import com.android.tiltcamera.app.Route
+import com.android.tiltcamera.camera.presentation.CameraScreen
+import com.android.tiltcamera.camera.presentation.CameraScreenRoot
+import com.android.tiltcamera.camera.presentation.CameraViewModel
+import com.android.tiltcamera.core.presentation.TiltCameraTheme
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        private val CAMERAX_PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA
+        )
+    }
+
+    private fun hasRequiredPermissions(): Boolean {
+        return CAMERAX_PERMISSIONS.all {
+            checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if(!hasRequiredPermissions()) {
+            ActivityCompat.requestPermissions(
+                this, CAMERAX_PERMISSIONS, 0
+            )
+        }
+
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.dark(
                 android.graphics.Color.TRANSPARENT,
@@ -36,33 +70,40 @@ class MainActivity : ComponentActivity() {
                 android.graphics.Color.TRANSPARENT,
             )
         )
+
         setContent {
             TiltCameraTheme {
-                val viewModel = viewModel<MainViewModel>()
+                val navController = rememberNavController()
 
+                NavHost(
+                    navController = navController,
+                    startDestination = Route.CameraGraph
+                ) {
 
-                    Column(modifier = Modifier.fillMaxSize()
-                        .background(brush = Brush.linearGradient(
-                            colors = listOf(Color(0xFFD5185C),Color(0xFF31338A )),
-                            start = Offset(0f, Float.POSITIVE_INFINITY),
-                            end = Offset(Float.POSITIVE_INFINITY, 0f)
-                        )),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center) {
-                        Column(modifier = Modifier
-                            .size(160.dp)
-                            .clip(CircleShape)
-                            .background(Color.White),
-                        verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally){
-                            Text(text = String.format(Locale.getDefault(), "Azimuth=%.2f°", viewModel.azimuth))
-                            Text(text = String.format(Locale.getDefault(), "Pitch=%.2f°", viewModel.pitch))
-                            Text(text = String.format(Locale.getDefault(), "Roll=%.2f°", viewModel.roll))
+                    navigation<Route.CameraGraph>(
+                        startDestination = Route.CameraScreen
+                    ) {
+
+                        composable<Route.CameraScreen>(
+                            exitTransition = { slideOutHorizontally() },
+                            popEnterTransition = { slideInHorizontally() }
+                        ) {
+
+                            val viewModel = hiltViewModel<CameraViewModel>()
+
+                            CameraScreenRoot(
+                                viewModel = viewModel
+                            )
 
                         }
 
+                        composable<Route.GalleryScreen>(
+                            exitTransition = { slideOutHorizontally() },
+                            popEnterTransition = { slideInHorizontally() }
+                        ) {
 
-
+                        }
+                    }
                 }
             }
         }
