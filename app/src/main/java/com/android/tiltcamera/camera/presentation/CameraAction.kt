a00d4e899f7424cf0b48aa16e7340dc900cff328
Loïc <loic.legros.dns@gmail.com>
2025-01-28 16:22:59 +0100

    improve camera screen UI and add bottom sheet for parameters

--- app/src/main/java/com/android/tiltcamera/camera/presentation/CameraAction.kt
+++ app/src/main/java/com/android/tiltcamera/camera/presentation/CameraAction.kt
@@ -1,8 +1,16 @@
 package com.android.tiltcamera.camera.presentation
 
 import android.graphics.Bitmap
+import com.android.tiltcamera.camera.domain.CameraResolution
+import com.android.tiltcamera.camera.domain.PicturesCollection
 
 
 sealed interface CameraAction {
     data class OnTakePhoto(val bitmap: Bitmap): CameraAction
+    data class SetAspectRatioMode(val aspectRatioMode: AspectRatioMode): CameraAction
+    data class OnPictureCollectionSelected(val pictureCollection: PicturesCollection): CameraAction
+    data class OnCameraResolutionSelected(val cameraResolution: CameraResolution): CameraAction
+    data class SetShowPictureInfo(val showPictureInfo: Boolean): CameraAction
+
+
 }
