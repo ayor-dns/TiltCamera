b77a6a6ec5c0eb5b5e29af1c726c28cc64e1dc2d
Loïc <loic.legros.dns@gmail.com>
2025-01-28 22:28:53 +0100

    add function to return empty PicturesCollection

--- app/src/main/java/com/android/tiltcamera/camera/domain/model/PicturesCollection.kt
+++ app/src/main/java/com/android/tiltcamera/camera/domain/model/PicturesCollection.kt
@@ -8,7 +8,19 @@ data class PicturesCollection(
     val creationTimestamp: Long,
     val cameraResolutionIdFK: Long,
     val aspectRatioMode: AspectRatioMode
-)
+) {
+    companion object {
+        fun newCollection(): PicturesCollection {
+            return PicturesCollection(
+                collectionId = -1,
+                name = "Nouvelle collection",
+                creationTimestamp = 0,
+                cameraResolutionIdFK = -1,
+                aspectRatioMode = AspectRatioMode.RATIO_4_3
+            )
+        }
+    }
+}
 
 
 
