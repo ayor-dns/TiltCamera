7e7e8c1151d7945051586723cb33c6dad0c57c9e
Loïc <loic.legros.dns@gmail.com>
2025-01-28 16:52:49 +0100

    feat: add utility function to determine text color based on background
    Added a utility function `getTextColor` that calculates the appropriate text color (black or white) based on the luminance of the provided background color. This ensures sufficient contrast for readability.

--- app/src/main/java/com/android/tiltcamera/core/domain/utils.kt
+++ app/src/main/java/com/android/tiltcamera/core/domain/utils.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
 
 fun openAppSettings(context: Context) {
     val intent = Intent(
@@ -13,3 +15,10 @@ fun openAppSettings(context: Context) {
     context.startActivity(intent)
 }
 
+fun getTextColor(backgroundColor: Color): Color {
+    val luminance1 = backgroundColor.luminance()
+    val luminance2 = 1.0f - luminance1
+    val contrastRatio = (luminance1 + 0.05) / (luminance2 + 0.05)
+    val textColor = if (contrastRatio > 5) Color.Black else Color.White
+    return textColor
+}
