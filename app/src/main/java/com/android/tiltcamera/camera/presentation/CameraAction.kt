9de0cdbc5c336ed3dc5fbc9edd925ff44564bb20
Loïc <loic.legros.dns@gmail.com>
2025-01-28 17:10:40 +0100

    refactor: update model package
    Updated the package for CameraResolution and PicturesCollection to `camera.domain.model`.

--- app/src/main/java/com/android/tiltcamera/camera/presentation/CameraAction.kt
+++ app/src/main/java/com/android/tiltcamera/camera/presentation/CameraAction.kt
@@ -1,14 +1,15 @@
 package com.android.tiltcamera.camera.presentation
 
 import android.graphics.Bitmap
-import com.android.tiltcamera.camera.domain.CameraResolution
-import com.android.tiltcamera.camera.domain.PicturesCollection
+import com.android.tiltcamera.camera.domain.AspectRatioMode
+import com.android.tiltcamera.camera.domain.model.CameraResolution
+import com.android.tiltcamera.camera.domain.model.PicturesCollection
 
 
 sealed interface CameraAction {
     data class OnTakePhoto(val bitmap: Bitmap): CameraAction
     data class SetAspectRatioMode(val aspectRatioMode: AspectRatioMode): CameraAction
-    data class OnPictureCollectionSelected(val pictureCollection: PicturesCollection): CameraAction
+    data class OnPictureCollectionSelected(val picturesCollection: PicturesCollection): CameraAction
     data class OnCameraResolutionSelected(val cameraResolution: CameraResolution): CameraAction
     data class SetShowPictureInfo(val showPictureInfo: Boolean): CameraAction
 
