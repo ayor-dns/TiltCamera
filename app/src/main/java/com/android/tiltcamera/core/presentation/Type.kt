8aa653553f0bbed1fe67b84a83e119161c0ac1d3
Loïc <loic.legros.dns@gmail.com>
2025-01-29 21:54:36 +0100

    add ui stuff

--- app/src/main/java/com/android/tiltcamera/core/presentation/Type.kt
+++ app/src/main/java/com/android/tiltcamera/core/presentation/Type.kt
@@ -3,8 +3,11 @@ package com.android.tiltcamera.core.presentation
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.android.tiltcamera.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,6 +34,21 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val SentinelPro = FontFamily(
+    Font(R.font.sentinelpro_light, FontWeight.Light),
+    Font(R.font.sentinelpro_lightitalic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.sentinelpro_book, FontWeight.Normal),
+    Font(R.font.sentinelpro_bookitalic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.sentinelpro_medium, FontWeight.Medium),
+    Font(R.font.sentinelpro_mediumitalic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.sentinelpro_semibold, FontWeight.SemiBold),
+    Font(R.font.sentinelpro_semibolditalic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.sentinelpro_bold, FontWeight.Bold),
+    Font(R.font.sentinelpro_bolditalic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.sentinelpro_black, FontWeight.Black),
+    Font(R.font.sentinelpro_blackitalic, FontWeight.Black, FontStyle.Italic)
 )
 
 val inputErrorTextStyle = TextStyle(
