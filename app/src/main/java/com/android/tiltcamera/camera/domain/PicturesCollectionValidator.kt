57d16c2fb157289cdf5f215761cf034ec35b237c
Loïc <loic.legros.dns@gmail.com>
2025-01-29 16:52:09 +0100

    feat: add picture collection name validator

---
+++ app/src/main/java/com/android/tiltcamera/camera/domain/PicturesCollectionValidator.kt
@@ -0,0 +1,24 @@
+package com.android.tiltcamera.camera.domain
+
+import com.android.tiltcamera.camera.domain.repository.CollectionRepository
+import com.android.tiltcamera.camera.domain.repository.PictureRepository
+import com.android.tiltcamera.core.domain.PictureCollectionNameError
+import com.android.tiltcamera.core.domain.Result
+
+class PicturesCollectionValidator(
+    private val collectionRepository: CollectionRepository
+) {
+
+    suspend fun validateName(name: String): Result<Unit, PictureCollectionNameError> {
+        if(name.isBlank()) return Result.Error(PictureCollectionNameError.Empty)
+
+        if(name.length > 100) return Result.Error(PictureCollectionNameError.TooLong)
+
+        if(!name.matches(Regex("^[a-zA-Z0-9_\\-/ ]+$"))) return Result.Error(PictureCollectionNameError.InvalidCharacters)
+
+        val collection = collectionRepository.getPictureCollectionByName(name)
+        if(collection != null) return Result.Error(PictureCollectionNameError.AlreadyExists)
+
+        return Result.Success(Unit)
+    }
+}
