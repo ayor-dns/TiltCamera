2f531d4be745836940995cb0bc57674560d6429a
Loïc <loic.legros.dns@gmail.com>
2025-01-29 16:58:00 +0100

    add use case to insert a new picture collection
    Added a new use case `InsertNewCollection` to insert a new `PicturesCollection` into the repository. The use case sets the creation timestamp to the current time.

---
+++ app/src/main/java/com/android/tiltcamera/camera/domain/use_case/InsertNewCollection.kt
@@ -0,0 +1,16 @@
+package com.android.tiltcamera.camera.domain.use_case
+
+import com.android.tiltcamera.camera.domain.model.PicturesCollection
+import com.android.tiltcamera.camera.domain.repository.CollectionRepository
+import com.android.tiltcamera.core.domain.Result
+import com.android.tiltcamera.core.domain.RoomError
+import com.android.tiltcamera.core.domain.getCurrentTimeInMillis
+
+class InsertNewCollection(
+    private val pictureCollectionRepository: CollectionRepository
+) {
+    suspend operator fun invoke(collection: PicturesCollection): Result<Long, RoomError> {
+        return pictureCollectionRepository.insertPicturesCollection(collection.copy(creationTimestamp = getCurrentTimeInMillis()))
+    }
+
+}
