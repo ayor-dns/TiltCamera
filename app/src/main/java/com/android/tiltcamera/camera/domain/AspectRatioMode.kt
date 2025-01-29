5e606db10643cc3bc9de821404a988227d3bfa47
Loïc <loic.legros.dns@gmail.com>
2025-01-29 16:51:53 +0100

    feat: add display name for aspect ratio mode
    Added display names for aspect ratio modes.
    - 16:9
    - 4:3
    - 0:0

--- app/src/main/java/com/android/tiltcamera/camera/domain/AspectRatioMode.kt
+++ app/src/main/java/com/android/tiltcamera/camera/domain/AspectRatioMode.kt
@@ -1,5 +1,7 @@
 package com.android.tiltcamera.camera.domain
 
-enum class AspectRatioMode {
-    RATIO_16_9, RATIO_4_3
+enum class AspectRatioMode(val displayName: String) {
+    RATIO_16_9("16:9"),
+    RATIO_4_3("4:3"),
+    RATIO_OTHER("0:0")
 }
