92cfaff5d8c2524ef5ebb71ab7d36872b5827671
Loïc <loic.legros.dns@gmail.com>
2025-02-07 19:39:32 +0100

    refactor: improve image preview and remove unused code
    Removed unnecessary imports and modified the image preview for better efficiency.

--- app/src/main/java/com/android/tiltcamera/camera/presentation/components/LastPicturePreview.kt
+++ app/src/main/java/com/android/tiltcamera/camera/presentation/components/LastPicturePreview.kt
@@ -4,10 +4,8 @@ import android.net.Uri
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,11 +16,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImagePainter
 import coil3.compose.rememberAsyncImagePainter
