2650d952117e7396bf5cad0b01eadbfd0bb2f8f4
Loïc <loic.legros.dns@gmail.com>
2025-02-07 19:37:56 +0100

    feat: add time formatting utilities
    Added `formatMillisToDateTime` function to format milliseconds into date and time strings based on specified patterns.
    Introduced `TimePattern` enum to define various date and time formatting patterns.

--- app/src/main/java/com/android/tiltcamera/core/presentation/utils.kt
+++ app/src/main/java/com/android/tiltcamera/core/presentation/utils.kt
@@ -6,6 +6,10 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.unit.Dp
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 fun Modifier.borderBottom(color: Color, width: Dp, dashLine: Boolean = false) = this.then(
     Modifier.drawBehind {
@@ -20,3 +24,29 @@ fun Modifier.borderBottom(color: Color, width: Dp, dashLine: Boolean = false) = 
         )
     }
 )
+
+fun formatMillisToDateTime(
+    millis: Long?,
+    timePattern: TimePattern = TimePattern.ISO_DATE_TIME,
+    returnedNullValue: String = " - "
+): String {
+    if (millis == null) {
+        return returnedNullValue
+    }
+    // Convert milliseconds to LocalDateTime
+    val dateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(millis), ZoneId.systemDefault())
+
+    // Create a DateTimeFormatter with the specified pattern
+    val formatter = DateTimeFormatter.ofPattern(timePattern.pattern)
+
+    // Format the LocalDateTime to a string with the desired pattern
+    return dateTime.format(formatter)
+}
+
+enum class TimePattern(val pattern: String) {
+    ISO_DATE_TIME("yyyy-MM-dd HH:mm:ss"),                  // 2024-03-20 12:34:56
+    NAMED_DAY_AND_NAMED_MONTH_YEAR_TIME("EEE d MMMM yyyy HH:mm:ss"),     // Mer. 20 mars 2024 12:34:56
+    SIMPLE_DATE("d MMMM yyyy"),                            // 20 mars 2024
+    DATE_ONLY("yyyy-MM-dd"),                               // 2024-03-20
+    TIME_ONLY("HH:mm:ss"),                                 // 12:34:56
+}
